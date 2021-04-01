fd488c8507401fa993b461032867ca5e2b1552f1
RondonLeonR <RondonLeonR@github.com>
2021-04-01 17:31:20 -0300

    Probando Heroku

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname + '/salade-juegos2'));
+app.use(express.static(__dirname + '/dist/SaladeJuegos2'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/SaladeJuegos2/index.html'));
 });
 
 console.log('Build successfull!!');
